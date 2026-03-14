0d1b1c72ac067f989f520f45f8aa603a8fda7a3e
mababajo <mababajo@gmail.com>
2026-03-14 02:37:25 +0100

    Add homepage with initail queue message

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Clinic Queue System is running!"
+   # Simple homepage showing queue message
+    return render_template_string("""
+        <h1>Welcome to the Clinic Queue System</h1>
+        <p>Current queue: 0 patients</p>
+    """)
 
 if __name__ == '__main__':
     app.run(debug=True)
